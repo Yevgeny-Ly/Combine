3ceafa6ab1ffb403b2f6706500106b509823b989
Yevgeny Ly <globchenko16@gmail.com>
2024-05-20 21:11:01 +0300

    - app init

---
+++ Combine_SwiftUI/Combine_SwiftUI/Combine_SwiftUIApp.swift
@@ -0,0 +1,15 @@
+//
+//  Combine_SwiftUIApp.swift
+//  Combine_SwiftUI
+//
+
+import SwiftUI
+
+@main
+struct Combine_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Combine_SwiftUI/Combine_SwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Combine_SwiftUI
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
