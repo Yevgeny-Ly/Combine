6e71ffb1a6d229029b239d7f2f8e4aa32630d5a6
Yevgeny Ly <globchenko16@gmail.com>
2024-05-24 13:01:56 +0300

    - доработано приложение toDoList

--- Combine_SwiftUI/Combine_SwiftUI/TodoListSwiftUI/TodoListView.swift
+++ Combine_SwiftUI/Combine_SwiftUI/TodoListSwiftUI/TodoListView.swift
@@ -55,39 +55,6 @@ struct TodoListView: View {
         .font(.title)
     }
 }
-
-final class TodoListViewModel: ObservableObject {
-    @Published var dataToList: [String] = []
-    @Published var value = ""
-    @Published var isHidden = false
-    
-    var selection = CurrentValueSubject<String, Never>("")
-    var cancellable: Set<AnyCancellable> = []
-    
-    init() {
-        selection
-            .flatMap({ item -> AnyPublisher<String, Never> in
-                if !item.isEmpty {
-                    return Just(item)
-                        .eraseToAnyPublisher()
-                } else {
-                    return Empty(completeImmediately: true)
-                        .eraseToAnyPublisher()
-                }
-            })
-            .sink { [unowned self] item in
-                dataToList.append(item)
-                value = ""
-                objectWillChange.send()
-            }
-            .store(in: &cancellable)
-    }
-    
-    func removeValue() {
-        dataToList.removeAll()
-        value = ""
-    }
-}
     
 #Preview {
     TodoListView()
